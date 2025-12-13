b83a4e8d59eba7e4723cde56f71dda70a4519e91
ccchibsss <ccchibsss@gmail.com>
2025-12-14 01:19:33 +0300

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,33 +1,25 @@
-#!/bin/bash
+from setuptools import setup, find_packages
 
-# Этот скрипт автоматически создаст виртуальное окружение с Python 3.11,
-# активирует его, обновит pip и установит все зависимости из requirements.txt
-
-# Проверка наличия Python 3.11
-if ! command -v python3.11 &> /dev/null
-then
-    echo "Python 3.11 не установлен. Установите его перед запуском этого скрипта."
-    exit 1
-fi
-
-# Создаем виртуальное окружение
-echo "Создаю виртуальное окружение..."
-python3.11 -m venv venv
-
-# Активируем окружение
-source venv/bin/activate
-
-# Обновляем pip
-echo "Обновляю pip..."
-pip install --upgrade pip
-
-# Устанавливаем зависимости из requirements.txt
-echo "Устанавливаю зависимости..."
-pip install -r requirements.txt
-
-# Установка detectron2
-echo "Устанавливаю detectron2..."
-pip install --upgrade pip
-pip install git+https://github.com/facebookresearch/detectron2.git@main
-
-echo "Настройка окружения завершена."
+setup(
+    name='watermark_remover',       # Название вашего проекта
+    version='0.1',
+    description='Watermark removal with Mask R-CNN',
+    author='Ваше Имя',              # Укажите ваше имя
+    packages=find_packages(),
+    install_requires=[
+        'torch>=1.7.0',
+        'torchvision>=0.8.0',
+        'streamlit',
+        'opencv-python',
+        'numpy',
+        'pillow',
+        'moviepy',
+        'torchvision',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
