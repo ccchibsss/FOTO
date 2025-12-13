c5787352d04ab3103b73b1fd3618c0da270033e8
ccchibsss <ccchibsss@gmail.com>
2025-12-14 01:22:15 +0300

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='watermark_remover',       # Название вашего проекта
+    name='watermark_remover',
     version='0.1',
     description='Watermark removal with Mask R-CNN',
-    author='Ваше Имя',              # Укажите ваше имя
+    author='Ваше Имя',
     packages=find_packages(),
     install_requires=[
-        'torch>=1.7.0',
+        'torch>=2.0.0,<2.1',
         'torchvision>=0.8.0',
-        'streamlit',
-        'opencv-python',
-        'numpy',
+        'streamlit==1.24.0',
+        'opencv-python==4.7.0.68',
+        'numpy==1.23.5',
         'pillow',
         'moviepy',
-        'torchvision',
+        'rich==14.2.0',
+        'pygments==2.19.2',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
